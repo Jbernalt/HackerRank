2ac31a5edfc70a6c8d7b8ff788f97ae7c3800886
AndreasLasses404 <56150531+AndreasLasses404@users.noreply.github.com>
2021-03-17 11:11:34 +0100

    Added groups to models

---
+++ Models/Groups/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackerRank.Models.Users;
+
+namespace HackerRank.Models.Groups
+{
+    public class Group 
+    {
+        public int GroupID { get; set; }
+        public int GitlabTeamId { get; set; }
+        public string GroupName { get; set; }
+        public int GroupRating { get; set; }
+        public List<User> Users { get; set; }
+
+
+    }
+}
---
+++ Models/Groups/GroupTransaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackerRank.Models.Transactions;
+
+namespace HackerRank.Models.Groups
+{
+    public class GroupTransaction
+    {
+        public int Value { get; set; }
+        public DateTime FetchDate { get; set; }
+        public Group Group { get; set; }
+        public Transaction Transaction { get; set; }
+
+    }
+}
