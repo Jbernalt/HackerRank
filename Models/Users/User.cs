6026445626b35d66cf521833f127420d32c59f89
Erik Rönnberg <erik.rnnberg@gmail.com>
2021-03-17 10:48:30 +0100

    Added User class

---
+++ Models/Users/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace HackerRank.Models.Users
+{
+    public class User : IdentityUser
+    {
+        public int GitLabId { get; set; }
+        public double MonthlyRating { get; set; }
+    }
+}
