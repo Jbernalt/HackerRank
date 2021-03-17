22d8dda60790b7e1911cacc3d65703f8a24cc1fd
AndreasLasses404 <56150531+AndreasLasses404@users.noreply.github.com>
2021-03-17 11:22:18 +0100

    Added composite key

--- Data/HackerRankContext.cs
+++ Data/HackerRankContext.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using HackerRank.Models.Groups;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HackerRank.Models.Transactions;
 
 namespace HackerRank.Data
 {
@@ -12,6 +13,19 @@ namespace HackerRank.Data
         public HackerRankContext(DbContextOptions<HackerRankContext> options)
             : base(options)
         {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<GroupTransaction>().HasKey(t => new { t.GroupId, t.TransactionId, t.FetchDate });
+            modelBuilder.Entity<Group>().HasData(
+                new Transaction() { TransactionId = 1, Description = "Commits", Points = 0.15 },
+                new Transaction() { TransactionId = 2, Description = "Issues opened", Points = 0.15 },
+                new Transaction() { TransactionId = 3, Description = "Issues solved", Points = 0.3 },
+                new Transaction() { TransactionId = 4, Description = "Merge requests", Points = 0.35 },
+                new Transaction() { TransactionId = 5, Description = "Comments", Points = 0.05 });
         }
     }
 }
--- Models/Groups/GroupTransaction.cs
+++ Models/Groups/GroupTransaction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using HackerRank.Models.Transactions;
@@ -8,6 +9,8 @@ namespace HackerRank.Models.Groups
 {
     public class GroupTransaction
     {
+        public int TransactionId { get; set; }
+        public int GroupId { get; set; }
         public int Value { get; set; }
         public DateTime FetchDate { get; set; }
         public Group Group { get; set; }
