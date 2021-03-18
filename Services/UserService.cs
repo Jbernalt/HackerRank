6d30bccfc58984bd0173d927f8d69a1e39b3c9ed
Jonathan Bernalt <jonathan@mecanit.se>
2021-03-18 11:36:53 +0100

    UserService stuff

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,8 +1,11 @@
-﻿using HackerRank.Responses;
+﻿using HackerRank.Data;
+using HackerRank.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace HackerRank.Services
@@ -14,7 +17,29 @@ namespace HackerRank.Services
 
     public class UserService : IUserService
     {
+        HackerRankContext _context;
+        public UserService(HackerRankContext context)
+        {
+            _context = context;
+        }
 
+        //public async Task GetUser()
+        //{
+        //    string baseUrl = "https://gitlab.com/api/v4/users/7603033/events";
+        //    List<UserResponse> userResponses = new List<UserResponse>();
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "tr3SszQyyabFd34jNbjU");
+        //        var response = await client.GetAsync(baseUrl);
+
+        //        var jsonResult = await response.Content.ReadAsStringAsync();
+
+        //        List<UserResponse> result = JsonSerializer.Deserialize<List<UserResponse>>(jsonResult);
+        //        userResponses.AddRange(result);
+        //    }
+
+        //}
 
     }
 }
